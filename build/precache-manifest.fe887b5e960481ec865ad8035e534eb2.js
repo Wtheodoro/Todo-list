00717f6e148cbee2c3a7e8078ced09c1ffd8838e
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-09-29 00:10:16 -0300

    up

---
+++ build/precache-manifest.fe887b5e960481ec865ad8035e534eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fea3a4955350607322537d71696549",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a487b7dd6a751d722e9",
+    "url": "/static/css/main.14e3e722.chunk.css"
+  },
+  {
+    "revision": "7be554fac38961d8f195",
+    "url": "/static/js/2.825f78d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.825f78d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a487b7dd6a751d722e9",
+    "url": "/static/js/main.4ff21c95.chunk.js"
+  },
+  {
+    "revision": "567d73b704ad15af71d6",
+    "url": "/static/js/runtime-main.2ce4b608.js"
+  }
+]);
